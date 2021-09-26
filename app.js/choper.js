9a379c81457503d2d38b94de7dfca257456e2305
carlos.alarcon <cvalarcon@gmail.com>
2021-09-26 20:25:09 -0300

    agregado index y 2 secciones nuevas de cub y choper

--- app.js/choper.js
+++ app.js/choper.js
@@ -2,24 +2,9 @@
 const CAR_PRODUCT = "cartProdcutsId";
 
 document.addEventListener("DOMContentLoaded",() => {
-loadProductsCub();
 loadProductsChop();
 });
 
-function getProductsCubDb() {
-   const url = "./dbMotosCub.json";
-   
-   return fetch (url)
-    .then(response => {
-    return response.json();
-   })
-    .then(result => {
-    return result;
-    })
-    .catch(err => {
-     console.log(err);
-    });
-}
 
 function getProductsChopDb() {
     const url = "./dbMotosChop.json";
@@ -36,35 +21,6 @@ function getProductsChopDb() {
      });
  }
  
-
-async function loadProductsCub(){
-const products = await getProductsCubDb();
-
-let html ="";
-products.forEach(product => {
-
-    html += `
-      <div class="col-lg-3 col-md-6 col-sm-12 product-container">
-        <div class="card product">
-            <img
-                src="${product.image}"
-                class="card-img-top"
-                alt="${product.brand}"
-            />
-            <div class="card-body">
-                <h5 class="card-title">${product.brand} - ${product.model}</h5>
-                 <p class="card-text">${product.extraInfo}</p>
-                <p class="card-text">U$D ${product.price}</p>
-                <button type="button" class=""btn btn-primary btn-cart">Comprar</button>
-            </div>
-        </div>
-      </div>
-      `;  
-});
-
-document.getElementsByClassName("products")[0].innerHTML = html;
-
-}
 
 
 async function loadProductsChop(){
@@ -98,8 +54,6 @@ async function loadProductsChop(){
 
 
 function openCloseCart() {
-
-    const containerCartProducts = document.getElementsByClassName("cart-products")[0];
-    
-    containerCartProducts
+   const btnCart = document.getElementsByClassName("cart-products")[0];
+   console.log(btnCart);
 }
---
+++ app.js/cub.js
@@ -0,0 +1,57 @@
+
+const CAR_PRODUCT = "cartProdcutsId";
+
+document.addEventListener("DOMContentLoaded",() => {
+loadProductsCub();
+});
+
+function getProductsCubDb() {
+   const url = "./dbMotosCub.json";
+   
+   return fetch (url)
+    .then(response => {
+    return response.json();
+   })
+    .then(result => {
+    return result;
+    })
+    .catch(err => {
+     console.log(err);
+    });
+}
+
+async function loadProductsCub(){
+const products = await getProductsCubDb();
+
+let html ="";
+products.forEach(product => {
+
+    html += `
+      <div class="col-lg-3 col-md-6 col-sm-12 product-container">
+        <div class="card product">
+            <img
+                src="${product.image}"
+                class="card-img-top"
+                alt="${product.brand}"
+            />
+            <div class="card-body">
+                <h5 class="card-title">${product.brand} - ${product.model}</h5>
+                 <p class="card-text">${product.extraInfo}</p>
+                <p class="card-text">U$D ${product.price}</p>
+                <button type="button" class=""btn btn-primary btn-cart">Comprar</button>
+            </div>
+        </div>
+      </div>
+      `;  
+});
+
+document.getElementsByClassName("products")[0].innerHTML = html;
+
+}
+
+
+
+function openCloseCart() {
+   const btnCart = document.getElementsByClassName("cart-products")[0];
+   console.log(btnCart);
+}
